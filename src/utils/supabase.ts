524204605093f1c32fd33303f3879083d402370f
CreatingInfinity <axelleltanal@gmail.com>
2025-08-26 04:03:06 +0800

    round 2

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://squtybkgujjgrxeqmrfs.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
