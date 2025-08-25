d396ce1bd82425aa11267b0008c65c2400c538ab
CreatingInfinity <axelleltanal@gmail.com>
2025-08-26 04:04:56 +0800

    testing

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://squtybkgujjgrxeqmrfs.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxdXR5YmtndWpqZ3J4ZXFtcmZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMjQ5MzAsImV4cCI6MjA3MTYwMDkzMH0.JKBFqotKygBRl1Ej060yeIRJbLzfTv7bnwAKDtwxHcY';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
